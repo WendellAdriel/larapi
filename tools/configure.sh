eeda0185f5823a485b82c53aa5e8f2e5454f4262
Wendell Adriel <wendelladriel.ti@gmail.com>
2020-03-30 18:28:34 +0100

    improved PHP-CS-FIXER integration

--- tools/configure.sh
+++ tools/configure.sh
@@ -4,8 +4,6 @@ echo "CONFIGURING LARAPI..."
 cp .env.example .env
 php artisan key:generate --ansi
 php artisan jwt:secret
-composer global require friendsofphp/php-cs-fixer
 clear
 ./vendor/bin/cghooks add --ignore-lock
-export PATH="$PATH:$HOME/.composer/vendor/bin"
 echo "LARAPI CONFIGURED!!!"
