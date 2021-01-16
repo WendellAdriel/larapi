f1f89e7bc4149e2cb31b92155db9122f03915101
Wendell Adriel <wendelladriel.ti@gmail.com>
2021-01-16 18:07:53 +0000

    updated migrations

--- database/migrations/2021_01_16_000002_create_users_table.php
+++ database/migrations/2021_01_16_000002_create_users_table.php
@@ -14,15 +14,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->id();
+            $table->uuid('uuid');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('active')->default(true);
-            $table->timestamp('last_login')->nullable();
+            $table->unsignedBigInteger('role_id');
             $table->json('settings')->nullable();
+            $table->timestamp('last_login')->nullable();
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
